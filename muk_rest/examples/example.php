028e9560e42b0be7d595c28480af9cc5ba5aa373
Mathias Markl <mathias.markl@mukit.at>
2018-12-15 12:02:26 +0100

    Update example.php

--- muk_rest/examples/example.php
+++ muk_rest/examples/example.php
@@ -1,32 +1,35 @@
 <?php
-	require_once('./vendor/autoload.php'); // https://github.com/thephpleague/oauth2-client
-	
-	$provider = new \League\OAuth2\Client\Provider\GenericProvider([
-		'clientId'                => 'LegacyApplicationFlowDemoClientKey',    // The client ID assigned to you by the provider
-		'clientSecret'            => 'LegacyApplicationFlowDemoClientSecret',   // The client password assigned to you by the provider
-		'redirectUri'             => 'https://app.swaggerhub.com/oauth2_redirect/',
-		'urlAuthorize'            => 'https://demo12.mukit.at/api/authentication/oauth2/authorize',
-		'urlAccessToken'          => 'https://demo12.mukit.at/api/authentication/oauth2/token',
-		'urlResourceOwnerDetails' => ''
-	]);
-	
-	try {
-		$accessToken = $provider->getAccessToken('password', [
-			'username' => 'demo',
-			'password' => 'demo'			
-		]);
-		$request = $provider->getAuthenticatedRequest(
-			'GET',
-			'https://demo12.mukit.at/api/user',
-			$accessToken
-		);
+  // HTTP Client (https://github.com/guzzle/guzzle)
+  // OAuth 2.0 Client (https://github.com/thephpleague/oauth2-client)
 
-		$client = new \GuzzleHttp\Client();
-		$response = $client->send($request);
-		$rawBody = $response->getBody()->getContents();
-		
-		print_r($rawBody);
-	} catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
-		exit($e->getMessage());
-	}
+  require_once('./vendor/autoload.php'); 
+  
+  $provider = new \League\OAuth2\Client\Provider\GenericProvider([
+    'clientId'                => 'LegacyApplicationFlowDemoClientKey',    // The client ID assigned to you by the provider
+    'clientSecret'            => 'LegacyApplicationFlowDemoClientSecret',   // The client password assigned to you by the provider
+    'redirectUri'             => 'https://app.swaggerhub.com/oauth2_redirect/',
+    'urlAuthorize'            => 'https://demo12.mukit.at/api/authentication/oauth2/authorize',
+    'urlAccessToken'          => 'https://demo12.mukit.at/api/authentication/oauth2/token',
+    'urlResourceOwnerDetails' => ''
+  ]);
+  
+  try {
+    $accessToken = $provider->getAccessToken('password', [
+      'username' => 'demo',
+      'password' => 'demo'      
+    ]);
+    $request = $provider->getAuthenticatedRequest(
+      'GET',
+      'https://demo12.mukit.at/api/user',
+      $accessToken
+    );
+
+    $client = new \GuzzleHttp\Client();
+    $response = $client->send($request);
+    $rawBody = $response->getBody()->getContents();
+    
+    print_r($rawBody);
+  } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
+    exit($e->getMessage());
+  }
 ?>
